eec7201a47557681d8882d4c298ce55959af4582
ProgSomel <somelahmed55@gmail.com>
2025-05-22 18:02:52 +0600

    union and intersection added

---
+++ 01 - Be A Typescript Technocrat/01 - Explore Basic Types of Typescript/1-10 Union and Intersection types/index.ts
@@ -0,0 +1,34 @@
+//! Union and Intersection types
+
+//? union type
+type FrontendDeveloper = "fakibazz" | 'juniorDeveloper';
+
+const newDeveloper : FrontendDeveloper = 'fakibazz'; //? we can use here only fakibazz or juniorDeveloper
+
+type User = {
+    name: string,
+    email: string,
+    gender: 'male' | 'female',
+    bloodGroup: 'A+' | 'B+',
+}
+
+type FullStackDeveloper = FrontendDeveloper |  'expertDeveloper';
+
+//? intersection type
+type FrontedDeveloper1 = {
+    skills: string[];
+    designation1: 'Frontend Developer';
+
+}
+type BackendDeveloper1 = {
+    skills: string[];
+    designation2: 'Backend Developer';
+}
+
+type FullstackDeveloper1 = FrontedDeveloper1 & BackendDeveloper1;
+
+const fullstackDeveloper: FullstackDeveloper1 = {
+    skills: ['html', 'css'],
+    designation1: 'Frontend Developer',
+    designation2: 'Backend Developer'
+}
