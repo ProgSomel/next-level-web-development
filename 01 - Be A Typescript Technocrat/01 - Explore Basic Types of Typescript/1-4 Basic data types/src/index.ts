c51593b2a167be2dfca8618f199f2a15eb346780
ProgSomel <somelahmed55@gmail.com>
2025-05-22 12:17:12 +0600

    basic data type added

---
+++ 01 - Be A Typescript Technocrat/01 - Explore Basic Types of Typescript/1-4 Basic data types/src/index.ts
@@ -0,0 +1,26 @@
+//! Primitive
+//? string
+let firstname = "Somel"; //? implicit data type(by own) 
+let lastname = "Ahmed"; //? explicit data type 
+
+//? number
+let roll: number = 12;
+
+//? boolean
+let isAdmin : boolean = true;
+
+//? undefined
+let x: undefined = undefined;
+
+//? null
+let y: null = null;
+
+//! Non Primitive
+//? Array
+let friends: string[] = ["somel", "ahmed"];
+let numbers: number[] = [50, 10];
+
+//? touple --> special type of array --> order --> type of valued
+let coordinates: [number, number] = [1, 5];
+let nameAge: [string, number, boolean] = ["somel", 10, true];
+
